405522188f8336159b4bf7a42c829faf53e0b13e
praparn <eva10409@gmail.com>
2019-05-26 13:38:00 +0700

    20190526133758

--- Workshop-2-6-Docker/init.sh
+++ Workshop-2-6-Docker/init.sh
@@ -5,27 +5,30 @@ declare -a SERVER_IPS=("192.168.100.10" "192.168.100.11" "192.168.100.10")
 timestamp=$(date +%Y%m%d_%H%M%S)
 script_path=/home/ubuntu/jmeter-workshop-062019/Workshop-2-6-Docker/jmeter
 result_path=/home/ubuntu/jmeter-workshop-062019/Workshop-2-6-Docker/result
-jmeter_path=/mnt/jmeter
+jmeter_scriptpath=/mnt/jmeter
+jmeter_resultpath=/mnt/result
 
 echo "Step 2: Create JMeter server"
 for IPADDR in "${SERVER_IPS[@]}"
 do
 	docker run --name $IPADDR \
 	-dit --net jmeternet --ip $IPADDR \
-    --mount type=bind,source=${volume_path},target=${jmeter_path} \
+    --mount type=bind,source=${result_path},target=${jmeter_resultpath} \
 	--rm jmeter -n -s \
 	-Jclient.rmi.localport=7000 -Jserver.rmi.localport=60000 \
-	-j ${jmeter_path}/server/slave_${timestamp}_${IPADDR:9:3}.log 
+	-j ${jmeter_resultpath}/server/slave_${timestamp}_${IPADDR:9:3}.log 
 done
 
 echo "Step 3: Create JMeter client"
-docker run --name JMETERCLIENT\
+docker run --name $CLIENT_IP \
   --net jmeternet --ip $CLIENT_IP \
+  --mount type=bind,source=${result_path},target=${jmeter_resultpath} \
+  --mount type=bind,source=${script_path},target=${jmeter_scriptpath} \
   -v "${volume_path}":${jmeter_path} \
   --rm jmeter -n -X \
   -Jclient.rmi.localport=7000 \
   -R $(echo $(printf ",%s" "${SERVER_IPS[@]}") | cut -c 2-) \
-  -t ${jmeter_path}/<jmx_script> \
-  -l ${jmeter_path}/client/result_${timestamp}.jtl \
-  -j ${jmeter_path}/client/jmeter_${timestamp}.log 
+  -t ${jmeter_scriptpath}/<jmx_script> \
+  -l ${jmeter_resultpath}/client/result_${timestamp}.jtl \
+  -j ${jmeter_resultpath}/client/jmeter_${timestamp}.log 
  
